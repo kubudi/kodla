4d95a11190d739ea462dc50715477e4447d06ff0
Mehmet Ali Gozaydin <kubudik@gmail.com>
2015-04-26 11:12:25 +0300

    add assets

--- src/main/scala/Workshop.scala
+++ src/main/scala/Workshop.scala
@@ -3,32 +3,95 @@ package asserts
 import scala.annotation.tailrec
 
 object Workshop {
-
-  //Sum of given numbers
-  def function1(num1: Int, num2: Int) = {
-    num1 + num2
+//=========================================================================
+//Sum of given two numbers
+  def function1(a: Int, b: Int) = {
+    -1
   }
-
-  //euler1
-  def function2() = {
-    (1 to 999).filter(i => i % 3 == 0 || i % 5 == 0).sum
+//=========================================================================
+//Calculate Hypotenuse
+//Hint: Use Math.sqrt()
+  def function2(a: Int, b: Int) = {
+    -1
   }
+//=========================================================================
+//Assignment
+  def functionAssign() = {
+    //assign
+    var mutable = Math.random
+    val immutable = Math.random
+    def func = Math.random
 
-  //euler2
-  def function3() = {
-    @tailrec
-    def iter(prev: Int, curr: Int, sum: Int): Int =
-      if(curr < 4000000) {
-        val newSum = 
-          if(curr % 2 == 0)
-            sum + curr
-          else
-            sum
+    //reassign
+    //mutable = 4
+    //immutable = 4
+    //func = 4
 
-        iter(curr, curr + prev, newSum)
-      }
-      else sum
 
-    iter(1, 1, 0)
+    //println("mutable: " + mutable)
+    //println("immutable: " + immutable)
+    //println("func: " + func)
   }
+//=========================================================================
+//Create Tuple
+//Hint: Representation => (value1, value2)
+  def function3(a: Int, b: Int) = {
+    -1
+  }
+//=========================================================================
+//Sum of a list
+  def function4(l: List[Int]) = {
+    -1
+  }
+//=========================================================================
+//Find sum of even numbers in list
+//Hint: filter
+  def function5(l: List[Int]) = {
+    -1
+  }
+//=========================================================================
+//Find sum of all numbers except the ones divisible by 5
+//Hint: filterNot
+  def function6(l: List[Int]) = {
+    -1
+  }
+//=========================================================================
+//Multiply all elements with 2
+//Hint: map
+  def function7(l: List[Int]) = {
+    -1
+  }
+//=========================================================================
+//Merge two lists into one
+//Hint: zip
+  def function8(name: List[String], surname: List[String]) = {
+    -1
+  }
+//=========================================================================
+//Currying function
+  def function9(a: Int) = {
+    -1
+  }
+//=========================================================================
+//Imperative euler2
+  def function10() = {
+    -1
+  }
+//=========================================================================
+//Recursive euler2
+  def function11() = {
+    -1
+  }
+//=========================================================================
+//Pattern matching
+abstract class Animal
+
+case class Cat extends Animal
+case class Dog extends Animal
+
+  def function12(a: Animal) = {
+    1
+  }
+//=========================================================================
+
 }
--- src/test/scala/WorkshopSpec.scala
+++ src/test/scala/WorkshopSpec.scala
@@ -16,37 +16,128 @@ class WorkshopSpec extends mutable.Specification {
 
   "Functions" should {
 
-    "function1 must return correct value" in {
-      val res = function1(3, 5)
-      val expected = 8
+    "function1 must calculate sum of 3 + 4" in {
+      val res = function1(3, 4)
+      val expected = 7
 
-      if(res == expected) {
+      if(res == expected)
         push("assert1", res.toString)
-      }
 
       res mustEqual expected
     }
 
-    "function2 must return correct value" in {
-      val res = function2()
-      val expected = 233168
+    "function2 must calculate Hypotenuse for 3 and 4" in {
+      val res = function2(3, 4)
+      val expected = 5
 
-      if(res == expected) {
+      if(res == expected)
         push("assert2", res.toString)
-      }
 
       res mustEqual expected
     }
 
-    "function3 must return correct value" in {
-      val res = function3()
+    "function3 must create a tuple with 3 and 4" in {
+      val res = function3(3, 4)
+      val expected = (3, 4)
+
+      if(res == expected)
+        push("assert3", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function4 must calculate sum of a list form 1 to 100" in {
+      val res = function4((1 to 100).toList)
+      val expected = 5050
+
+      if(res == expected)
+        push("assert4", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function5 must calculate sum of even numbers in a list form 1 to 100" in {
+      val res = function5((1 to 100).toList)
+      val expected = 2550
+
+      if(res == expected)
+        push("assert5", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function6 must calculate sum of numbers not divisible by 5" in {
+      val res = function6((1 to 100).toList)
+      val expected = 4000
+
+      if(res == expected)
+        push("assert6", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function7 must multiply all values in the list with 2" in {
+      val res = function7((1 to 5).toList)
+      val expected = List(2,4,6,8,10)
+
+      if(res == expected)
+        push("assert7", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function8 must merge two list into one" in {
+      val res = function8(List("Ali", "Veli", "Hasan"), List("Oz", "Ak", "Can"))
+      val expected = List(("Ali","Oz"), ("Veli","Ak"), ("Hasan","Can"))
+
+      if(res == expected)
+        push("assert8", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function9 calculate sum of square and cube of a given number" in {
+      val res = function9(2)
+      val expected =  12
+
+      if(res == expected)
+        push("assert9", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function10 should solve project euler 2 imperatively" in {
+      val res = function10()
+      val expected =  4613732
+
+      if(res == expected)
+        push("assert10", res.toString)
+
+      res mustEqual expected
+    }
+
+    "function11 should solve project euler 2 recursively" in {
+      val res = function11()
       val expected = 4613732
 
-      if(res == expected) {
-        push("assert3", res.toString)
-      }
+      if(res == expected)
+        push("assert11", res.toString)
 
       res mustEqual expected
+    }
+
+    "function12 bark and meow accordingly" in {
+      val res = function12(new Dog)
+      val expected = "hav"
+
+      val res2 = function12(new Cat)
+      val expected2 = "miyav"
+
+      if(res == expected && res2 == expected2)
+        push("assert12", res.toString)
+
+      res mustEqual expected
+      res2 mustEqual expected2
     }
   }
 
